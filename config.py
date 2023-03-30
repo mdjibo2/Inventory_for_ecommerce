eed36f0428757313bf3c0cd6b35a83fd360e3987
mdjibo2 <justpourmestest00@gmail.com>
2023-03-30 09:21:45 +0000

    server docs

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    DOLIBARR_API_URL = 'https://macompta-dlb-free.easya.solutions/doliprod'
-    DOLIBARR_API_KEY = 'J6qNKhU62K1Xvl8sQ9k2JUWw19Jmwoi5'
+    DOLIBARR_API_URL = 'https://bosscompta.saas2.doliondemand.fr/api/admin/index.php'
+    DOLIBARR_API_KEY = 'FkFs05V8BGxBP4Pj6Juzx5oA90bD3i5r'
     SHOPIFY_API_KEY = '539ffca340b66ff2362f7ad93b02adfa'
     SHOPIFY_API_PASSWORD = '54f4728577b1b7a948b9dc04838af737'
     SHOPIFY_STORE_URL = 'https://automateaccounting.myshopify.com/admin'
